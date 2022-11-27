991a234a8d0174ec033dd74e988bd1a62c4ea039
Kunegunda <kosekkuengunda@wp.pl>
2022-11-27 12:28:13 +0100

    Add new files

---
+++ components/project17.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Hello woeld!');
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
+
+
+    document.addEventListener('mousewheel', function (e) {
+        if (isThrottled) return;
+        isThrottled = true;
+        setTimeout(function () {
+            isThrottled = false;
+        }, 1000);
+
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastection = currentSectionIndex === sections.length - 1;
+            if (isLastection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+        console.log(currentSectionIndex);
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    })
+
+})
